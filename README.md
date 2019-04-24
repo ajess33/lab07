63ebba1c043a6baa371f55aacef24678171e8667
Austin Jess <ajess33@gmail.com>
2019-04-24 09:13:37 -0500

    copied files over

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# lab07
+# lab-06-back-end
+
+Number and name of feature: Locations
+
+Estimate of time needed to complete: 1 hour 30 minutes
+
+Start time: 9:30
+
+Finish time: 10:50
+
+Actual time needed to complete: 1 hour 20 minutes
+
+---
+
+Number and name of feature: Weather
+
+Estimate of time needed to complete: 1 hour
+
+Start time: 11:00
+
+Finish time: 11: 40
+
+Actual time needed to complete: 40 minutes
+
+---
+
+Number and name of feature: Errors
+
+Estimate of time needed to complete: 30 minutes
+
+Start time: 11:45
+
+Finish time: 12:10
+
+Actual time needed to complete: 25 minutes
---
+++ server.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
+const port = process.env.PORT || 3000;
+
+app.get('/location', (req, res) => {
+  try {
+    res.send(findLatLong(req.query.data));
+  } catch (error) {
+    handleErrors(res);
+  }
+});
+
+app.get('/weather', (req, res) => {
+  try {
+    res.send(getWeather());
+  } catch (error) {
+    handleErrors(res);
+  }
+});
+
+const getWeather = () => {
+  const darkSkyData = require('./data/darksky.json');
+  let weatherArr = [];
+
+  darkSkyData.daily.data.forEach((dailySet) => {
+    const weather = new Weather(dailySet);
+    weatherArr.push(weather);
+  });
+  return weatherArr;
+};
+
+function Weather(data) {
+  this.forecast = data.summary;
+  this.time = new Date(data.time * 1000).toString().slice(0, 15);
+}
+
+const findLatLong = (query) => {
+  const geoData = require('./data/geo.json');
+  const location = new Location(query, geoData);
+  return location;
+};
+
+function Location(query, data) {
+  (this.searchQuery = query),
+  (this.formattedQuery = data.results[0].formatted_address),
+  (this.latitude = data.results[0].geometry.location.lat),
+  (this.longitude = data.results[0].geometry.location.lng);
+}
+
+// ERROR HANDLING
+
+const handleErrors = (res) => {
+  res
+    .status(500)
+    .send({ Status: 500, responseText: 'Sorry, something went wrong!' });
+};
+
+app.listen(port, () => console.log('Listening!!!'));
